047085ed01230e42879597817a4501806727326e
justinacelentano <jcelent1@ramapo.edu>
2018-09-22 00:37:00 -0400

    added rewards object

---
+++ backend/src/main/java/com/codeforgood/team10/controller/RewardController.java
@@ -0,0 +1,47 @@
+package com.codeforgood.team10.controller;
+
+import com.codeforgood.team10.data.Reward;
+import com.codeforgood.team10.data.RewardRepository;
+import com.codeforgood.team10.data.User;
+import com.codeforgood.team10.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+/**
+ * Created by justina on 9/21/18.
+ */
+
+@RestController
+@RequestMapping(path = "/reward")
+public class RewardController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RewardRepository rewardRepository;
+
+    @GetMapping ("/points/add/{value}")
+        public boolean one(@RequestHeader(value="phoneNumber") long phoneNumber, @PathVariable int value) {
+            User user = userRepository.findByPhoneNumber(phoneNumber);
+
+            user.setPoints(user.getPoints() + value);
+
+            userRepository.save(user);
+
+            return true;
+    }
+
+
+    @PostMapping(path = "/create")
+    public void createReward(@RequestBody Reward reward) {
+        rewardRepository.save(reward);
+    }
+
+    public List<Reward> findAll() {
+        return rewardRepository.findAll();
+    }
+}
