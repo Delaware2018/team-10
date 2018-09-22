5696d8d3ec983fbe77adbaf20870979cd098352c
Samuel Beluch <sambeluch@yahoo.com>
2018-09-21 23:16:17 -0400

    removes use of Item object in donation

--- backend/src/main/java/com/codeforgood/team10/data/Donation.java
+++ backend/src/main/java/com/codeforgood/team10/data/Donation.java
@@ -10,18 +10,47 @@ import java.util.Date;
 
 @Entity
 public class Donation {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String location;
     private Date date;
     private long userID;
+    private String item;
+    private int quantity;
+    private float price;
 
-    public Donation(String location, Date date, Items items, long id) {
+    public Donation(String location, Date date, String item, long id, int quantity, float price) {
         this.location = location;
         this.date = date;
         this.userID = id;
+        this.item = item;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 
     public long getId() {
