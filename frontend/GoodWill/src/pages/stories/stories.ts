22165c1d4eedde6c2643e3892630309bdc2caf63
henrylkirk <henry@the-kirks.com>
2018-09-22 02:41:51 -0400

    Connected user provider

--- frontend/GoodWill/src/pages/stories/stories.ts
+++ frontend/GoodWill/src/pages/stories/stories.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-stories',
+  templateUrl: 'stories.html'
 })
-export class ContactPage {
+export class StoriesPage {
 
   constructor(public navCtrl: NavController) {
 
